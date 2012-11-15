2e0c939dfcf64b7bb6511b4248030e2d6a4ebd2a
Eugene Vigdorchik <eugene.vigdorchik@gmail.com>
2012-11-15 19:19:51 +0400

    Use java.lang.Throwable.setStackTrace. Proper fix for #543.

--- testing/agent/src/main/java/sbt/ForkMain.java
+++ testing/agent/src/main/java/sbt/ForkMain.java
@@ -50,15 +50,13 @@ public class ForkMain {
 	}
 	static class ForkError extends Exception implements Serializable {
 		private String originalMessage;
-		private StackTraceElement[] originalStackTrace;
 		private ForkError cause;
 		ForkError(Throwable t) {
 			originalMessage = t.getMessage();
-			originalStackTrace = t.getStackTrace();
+			setStackTrace(t.getStackTrace());
 			if (t.getCause() != null) cause = new ForkError(t.getCause());
 		}
 		public String getMessage() { return originalMessage; }
-		public StackTraceElement[] getStackTrace() { return originalStackTrace; }
 		public Exception getCause() { return cause; }
 	}
 	static class ForkEvent implements Event, Serializable {
